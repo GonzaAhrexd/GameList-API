567590f479d0529b6da1a396e3f55fe582269c1d
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-12-18 12:44:53 -0300

    First commit

---
+++ GamesAPI/Controllers/PingController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet("getPing")]
+        public IActionResult Ping()
+        {
+            return Ok("Pong");
+        }
+      
+    [HttpPost("postContent")]
+    public IActionResult PostContent([FromBody] object bodyContent)
+    {
+        if(bodyContent == null)
+        {
+            return BadRequest("Invalid request body");
+        }
+
+
+        // Aquí puedes manipular el contenido del cuerpo según sea necesario
+        return Ok(bodyContent);
+    }
+
+    }
+
+
+
+
+}
+
+
---
+++ GamesAPI/Entities/Game.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace GamesAPI.Entities
+{
+    public class Game
+    {   
+        [BsonId]
+        [BsonElement("id"), BsonRepresentation(BsonType.ObjectId)]
+        public string? _id { get; set; }
+
+        [BsonElement("nombre")]
+        public string? nombre { get; set; }
+        [BsonElement("imagen")]
+        public string? imagen { get; set; }
+        [BsonElement("genero")]
+        public string? genero { get; set; }
+
+
+    }
+}
